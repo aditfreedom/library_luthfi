9c148b0d9ce3c173918e33c874ac85051938a39d
Adit <37005476+aditfreedom@users.noreply.github.com>
2024-01-27 09:13:16 +0700

    Update UserSeeder & Books Model

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Books;
+use App\Models\Member;
+use App\Models\Publisher;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        //$books = Member::with('user')->get();
+        //$books = Book::with('publisher')->get();
+        $publishers = Publisher::with('books')->get();
+
+        return $publishers;
+        return view('home');
+    }
+}
